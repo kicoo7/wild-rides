9d189c0cbeca9f233fbded5f1ceb6b41ffb701cd
kicoo7 <rajchevski.krste@live.com>
2018-09-04 16:13:16 +0200

    Updates list unicorns lambda functions

--- list/index.js
+++ list/index.js
@@ -1,14 +1,32 @@
 const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient({});
+const { UNICORNS_TABLE_NAME } = process.env;
 
-exports.lambda_handler = (event, context, callback) => {
+exports.lambda_handler = async (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
 
-    var message = "Hello World!";
+    try {
+        const params = {
+            TableName: UNICORNS_TABLE_NAME
+        };
 
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-            Output: message
-        })
-    });
+        const unicorns = await documentClient.scan(params).promise();
+
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({
+                Output: 'Test'
+            })
+        });
+    }
+    catch (err) {
+        console.log("\n Error:", err);
+        callback(null, {
+            statusCode: 500,
+            body: JSON.stringify({
+                Output: "Something bad happened."
+            })
+        });
+    }
+
 };
